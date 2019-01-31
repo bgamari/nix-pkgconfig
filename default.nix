18557e9cf689c6ee118fc8cdff41206262965836
Ben Gamari <ben@smart-cactus.org>
2019-01-31 17:45:30 -0500

    Remove nixified database creation workflow

--- default.nix
+++ default.nix
@@ -1,18 +1,6 @@
-{ nixpkgs ? (import <nixpkgs> {}), pkg-index ? null }:
+{ nixpkgs ? (import <nixpkgs> {}) }:
 
 with nixpkgs;
 let 
   nix-pkgconfig = callPackage ./package.nix {};
-
-  # This currently isn't used since it's quite inconvient due to nix-index's
-  # dependence on Internet access.
-  pc-database = import ./build-database.nix { inherit nixpkgs pkg-index; };
-  wrapped = runCommand "nix-pkgconfig-wrapped" {
-    nativeBuildInputs = [ makeWrapper ];
-    passthru = { inherit database; };
-  } ''
-    mkdir -p $out/bin
-    makeWrapper ${nix-pkgconfig}/bin/pkg-config $out/bin/pkg-config \
-      --suffix NIX_PKGCONFIG_DATABASES : ${./default-database.json}:${pc-database}/database.json
-  '';
 in nix-pkgconfig
