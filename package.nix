646ecce37ae5453d864c3c6891246d7d05d3fbd0
Ben Gamari <ben@smart-cactus.org>
2019-06-05 08:58:27 -0400

    Prefer local builds

--- package.nix
+++ package.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
   name = "nixpkgs-pkgconfig";
   buildInputs = [ python3 ];
   src = ./.;
+  preferLocalBuilds = true;
   installPhase = ''
     mkdir -p $out/bin
     cp pkg-config $out/bin/pkg-config
